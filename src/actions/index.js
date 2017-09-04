a0062f58f87e75a0e7f84c27343752149117526a
Nikhil Mandlik <nikhil_mandlik@apple.com>
2017-09-04 14:46:34 -0700

    wip: updated some clean up

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,7 @@
 import Axios from 'axios';
 
-const API_KEY = "7905bb757a229109";
+const API_KEY = '7905bb757a229109';
 const ROOT_URL = `http://api.wunderground.com/api/${API_KEY}`;
-
-// Find city from search Term and the use 
-// http://autocomplete.wunderground.com/aq?query=new%20york&c=US
-// Final:
-// http://api.wunderground.com/api/7905bb757a229109/hourly/q/zmw:10001.8.99999.json
-
-// http://api.wunderground.com/api/7905bb757a229109/hourly/q/CA/San_Francisco.json
-// http://api.wunderground.com/api/7905bb757a229109/hourly/q/San%20Francisco,US.json
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
@@ -20,5 +12,5 @@ export function fetchWeather(city) {
     return {
         type: FETCH_WEATHER, 
         payload: response
-    }
+    };
 }
--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -19,7 +19,7 @@ class SearchBar extends Component {
     onFormSubmit(event) {
         event.preventDefault();
 
-        // this.props.fetchWeather(this.state.term);
+        this.props.fetchWeather(this.state.term);
         this.setState({term: ''});
     }
 
@@ -39,7 +39,7 @@ class SearchBar extends Component {
     }
 }
 
-const mapDispatchToProps = (dispatch) => {
+function mapDispatchToProps(dispatch) {
     return bindActionCreators({ fetchWeather }, dispatch)
 }
 
